cfa26b94712a21121551d69bbec58b2a8d63505b
jack.freeman@databricks.com <jack.freeman@databricks.com>
2025-07-03 09:41:14 +0000

    added startup logic

--- demo_setup/model_deploy_jobs/create-deployment-job.py
+++ demo_setup/model_deploy_jobs/create-deployment-job.py
@@ -1,70 +1,99 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC # INSTRUCTIONS
-# MAGIC Running this notebook will create a Databricks Job templated as a MLflow 3.0 Deployment Job. This job will have three tasks: Evaluation, Approval_Check, and Deployment. The Evaluation task will evaluate the model on a dataset, the Approval_Check task will check if the model has been approved for deployment using UC Tags and the Approval button in the UC Model UI, and the Deployment task will deploy the model to a serving endpoint.
-# MAGIC
-# MAGIC 1. Copy the example deployment jobs template notebooks ([AWS](https://docs.databricks.com/aws/mlflow/deployment-job#example-template-notebooks) | [Azure](https://learn.microsoft.com/azure/databricks/mlflow/deployment-job#example-template-notebooks) | [GCP](https://docs.databricks.com/gcp/mlflow/deployment-job#example-template-notebooks)) into your Databricks Workspace.
-# MAGIC 2. Create a UC Model or use an existing one. For example, see the MLflow 3 examples ([AWS](https://docs.databricks.com/aws/mlflow/mlflow-3-install#example-notebooks) | [Azure](https://learn.microsoft.com/azure/databricks/mlflow/mlflow-3-install#example-notebooks) | [GCP](https://docs.databricks.com/gcp/mlflow/mlflow-3-install#example-notebooks)).
-# MAGIC 3. Update the TODOs/values in the next cell before running the notebook.
-# MAGIC 4. After running the notebook, the created job will not be connected to any UC Model. You will still need to **connect the job to a UC Model** in the UC Model UI as indicated in the documentation ([AWS](https://docs.databricks.com/aws/mlflow/deployment-job#connect) | [Azure](https://learn.microsoft.com/azure/databricks/mlflow/deployment-job#connect) | [GCP](https://docs.databricks.com/gcp/mlflow/deployment-job#connect)).
+# MAGIC ### Deployment Job Creation
+# MAGIC This notebook cell when run will create a deployment job for each of our models we have just deployed.
+# MAGIC See documentation at: [Documentation](https://docs.databricks.com/aws/en/mlflow/deployment-job)
 
 # COMMAND ----------
 
-# MAGIC %run ../../config
+# MAGIC %pip install mlflow
+# MAGIC dbutils.library.restartPython() 
 
 # COMMAND ----------
+
+# MAGIC %run ../../00.set_variables
+
+# COMMAND ----------
+
+from pathlib import Path
 
 # TODO: Update these values as necessary
-model_name = f"{catalog_name}.{schema_name}.fe_model" # The name of the already created UC Model
-job_name = "mining_demo_si_model_deployment_job" # The desired name of the deployment job
+
 
 # TODO: Create notebooks for each task and populate the notebook path here, replacing the INVALID PATHS LISTED BELOW.
 # These paths should correspond to where you put the notebooks templated from the example deployment jobs template notebook
 # in your Databricks workspace.
-evaluation_notebook_path = "/Workspace/Shared/iron_ore_precessing_demo/demo_setup/model_deploy_jobs/evaluation"
-approval_notebook_path = "/Workspace/Shared/iron_ore_precessing_demo/demo_setup/model_deploy_jobs/approval"
-deployment_notebook_path = "/Workspace/Shared/iron_ore_precessing_demo/demo_setup/model_deploy_jobs/deployment"
+
+current_notebook_path = dbutils.entry_point.getDbutils().notebook().getContext().notebookPath().get()
+# Use pathlib to handle path manipulation robustly
+current_notebook_dir = Path(current_notebook_path).parent
+
+evaluation_notebook_path = f"{current_notebook_dir}/evaluation"
+approval_notebook_path = f"{current_notebook_dir}/approval"
+deployment_notebook_path = f"{current_notebook_dir}/deployment"
 
 # COMMAND ----------
 
 # Create job with necessary configuration to connect to model as deployment job
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.service import jobs
+import mlflow
+from mlflow.tracking.client import MlflowClient
 
+client = MlflowClient(registry_uri="databricks-uc")
 w = WorkspaceClient()
-job_settings = jobs.JobSettings(
-    name=job_name,
-    tasks=[
-        # jobs.Task(
-        #     task_key="Evaluation",
-        #     notebook_task=jobs.NotebookTask(notebook_path=evaluation_notebook_path),
-        #     max_retries=0,
-        # ),
-        jobs.Task(
-            task_key="Approval_Check",
-            notebook_task=jobs.NotebookTask(
-                notebook_path=approval_notebook_path,
-                base_parameters={"approval_tag_name": "{{task.name}}"}
-            ),
-            #depends_on=[jobs.TaskDependency(task_key="Evaluation")],
-            max_retries=0,
-        ),
-        jobs.Task(
-            task_key="Deployment",
-            notebook_task=jobs.NotebookTask(notebook_path=deployment_notebook_path),
-            depends_on=[jobs.TaskDependency(task_key="Approval_Check")],
-            max_retries=0,
-        ),
-    ],
-    parameters=[
-        jobs.JobParameter(name="model_name", default=model_name),
-        jobs.JobParameter(name="model_version", default=""),
-    ],
-    queue=jobs.QueueSettings(enabled=True),
-    max_concurrent_runs=1,
-)
 
-created_job = w.jobs.create(**job_settings.__dict__)
-print("Use the job name " + job_name + " to connect the deployment job to the UC model " + model_name + " as indicated in the UC Model UI.")
-print("\nFor your reference, the job ID is: " + str(created_job.job_id))
-print("\nDocumentation: \nAWS: https://docs.databricks.com/aws/mlflow/deployment-job#connect \nAzure: https://learn.microsoft.com/azure/databricks/mlflow/deployment-job#connect \nGCP: https://docs.databricks.com/gcp/mlflow/deployment-job#connect")
+def create_deployment_job(model_element:str):
+    model_name = f"{catalog_name}.{schema_name}.{model_element}_model" # The name of the already created UC Model
+    job_name = f"demo_mining_processing_{model_element}_model_deploy" # The desired name of the deployment job
+    job_settings = jobs.JobSettings(
+        name=job_name,
+        tasks=[
+            #TODO: fix up evaluation
+            # jobs.Task(
+            #     task_key="Evaluation",
+            #     notebook_task=jobs.NotebookTask(notebook_path=evaluation_notebook_path),
+            #     max_retries=0,
+            # ),
+            jobs.Task(
+                task_key="Approval_Check",
+                notebook_task=jobs.NotebookTask(
+                    notebook_path=approval_notebook_path,
+                    base_parameters={"approval_tag_name": "{{task.name}}"}
+                ),
+                #depends_on=[jobs.TaskDependency(task_key="Evaluation")],
+                max_retries=0,
+            ),
+            jobs.Task(
+                task_key="Deployment",
+                notebook_task=jobs.NotebookTask(notebook_path=deployment_notebook_path),
+                depends_on=[jobs.TaskDependency(task_key="Approval_Check")],
+                max_retries=0,
+            ),
+        ],
+        parameters=[
+            jobs.JobParameter(name="model_name", default=model_name),
+            jobs.JobParameter(name="model_version", default=""),
+        ],
+        queue=jobs.QueueSettings(enabled=True),
+        max_concurrent_runs=1,
+    )
+
+    created_job = w.jobs.create(**job_settings.__dict__)
+
+    print(f"job created with id: {created_job.job_id}")
+    print(f"applying to model {model_name}")
+    try:
+        if client.get_registered_model(model_name):
+            client.update_registered_model(model_name, deployment_job_id=created_job.job_id)
+            print("Model updated")
+    except mlflow.exceptions.RestException:
+        print("Model does not exist")
+        client.create_registered_model(model_name, deployment_job_id=created_job.job_id)
+
+    print(f"Deployment Job {job_name} Created for Model: {model_name}")
+
+#create the two model deployment jobs
+for model_name in ["fe", "si"]:    create_deployment_job(model_name)
+
+
